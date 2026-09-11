60f1dbd54d47b83842609f89f4c0ea76779bfbab
aluz <cyjhi@qq.com>
2026-09-11 11:13:02 +0800

    方案2：保留标签加密，all页面列出全部文章

--- scripts/generate_all.js
+++ scripts/generate_all.js
@@ -1,11 +1,15 @@
 // scripts/generate_all.js
 hexo.extend.generator.register('all-posts', function(locals){
-  const posts = locals.all_posts.sort('-date');
+  // Collection调用toArray()转为普通数组，再合并
+  const publicPosts = locals.posts.toArray();
+  const hiddenPosts = locals.hidden_posts.toArray();
+  const posts = publicPosts.concat(hiddenPosts).sort((a,b) => b.date - a.date);
+
   let listHtml = `<!DOCTYPE html>
 <html>
 <head>
 <meta charset="utf-8">
-<title>全部文章</title>
+<title>全部文章列表</title>
 </head>
 <body>
 <h1>全部文章列表</h1>
@@ -13,7 +17,6 @@ hexo.extend.generator.register('all-posts', function(locals){
 `;
   posts.forEach(post => {
     const title = post.title;
-    // 加前置 /，从网站根目录开始，避免all页面内路径拼接错误
     const url = "/" + post.path;
     const tags = post.tags ? post.tags.map(t => t.name) : [];
     const isPrivate = tags.includes("private");
